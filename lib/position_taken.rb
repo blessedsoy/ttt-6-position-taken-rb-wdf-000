d26ec216752bb57cf247c5c05f25eaa9822f70ac
blessedsoy <unmigong@gmail.com>
2016-06-21 13:41:10 -0400

    Tic Tac Toe Positon Taken method

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+
+
+if board[index] == "X" || board[index] == "O" 
+   return true
+elsif board[index] == " " || board[index] == "  "
+  return false
+elsif index == nil || ""
+  return false
+
+
+end
+
+
+
+end
+
